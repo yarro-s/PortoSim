033c0eb0a86f2c0472bee788bf058bb59c14b0e5
yarro <yarro@localhost>
2022-06-09 15:19:03 +0300

    add metrics for series

--- src/main/java/money/portosim/Metrics.java
+++ src/main/java/money/portosim/Metrics.java
@@ -1,10 +1,47 @@
 package money.portosim;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import money.portosim.containers.Quote;
+import money.portosim.containers.QuoteSeries;
+import money.portosim.containers.core.Series;
 
 public class Metrics {
+    
+    public static Quote volatility(QuoteSeries quoteSeries) {
+        return quoteSeries.mapSeries(Metrics::volatility);
+    }
+    
+    public static Quote average(QuoteSeries quoteSeries) {    
+        return quoteSeries.mapSeries(Metrics::average);
+    }
+    
+    public static Quote cummulativeGrowthRate(QuoteSeries quoteSeries) {
+        return quoteSeries.mapSeries(Metrics::cummulativeGrowthRate); 
+    }
+    
+    public static Quote totalReturn(QuoteSeries quoteSeries) {
+        return quoteSeries.mapSeries(Metrics::totalReturn);
+    }
+    
+    public static double volatility(Series<Double> series) {
+        return volatility(series.values());
+    }
+    
+    public static double average(Series<Double> series) {
+        return average(series.values()); 
+    }
+    
+    public static double cummulativeGrowthRate(Series<Double> series) {
+        return cummulativeGrowthRate(new ArrayList<>(series.values())); 
+    }
+        
+    public static double totalReturn(Series<Double> series) {
+        return totalReturn(new ArrayList<>(series.values()));
+    }
 
-    public static double volatility(List<Double> values) {
+    public static double volatility(Collection<Double> values) {
         final double nValues = values.size();
         final double averageValue = average(values);
 
@@ -13,7 +50,7 @@ public class Metrics {
         return Math.sqrt(squareDiffs.sum() / nValues);
     }
 
-    public static double average(List<Double> values) {
+    public static double average(Collection<Double> values) {
         return values.stream().mapToDouble(Double::doubleValue).average().orElseThrow();
     }
 
@@ -29,5 +66,8 @@ public class Metrics {
                 valueAtEnd = values.get(values.size() - 1);
 
         return valueAtEnd / valueAtStart;
+    }
+
+    private Metrics() {
     }
 }
---
+++ src/test/java/money/portosim/MetricsTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package money.portosim;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import money.portosim.containers.Quote;
+import money.portosim.containers.QuoteSeries;
+import money.portosim.containers.sources.QuoteSeriesCSVSource;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author yarro
+ */
+public class MetricsTest {
+    private final String csvDataSourcePath = "src/test/resources/simple.csv"; 
+    private QuoteSeries quoteSeries; 
+    
+    @BeforeClass
+    public void setup() throws FileNotFoundException, IOException {
+        var quoteSeriesSource = new QuoteSeriesCSVSource(new FileReader(csvDataSourcePath));
+        quoteSeries = new QuoteSeries(quoteSeriesSource);
+    }
+    
+    @Test
+    public void seriesVolatility() {
+           
+        var vol = Metrics.volatility(quoteSeries);
+        
+        Assert.assertEquals(vol.get("A"), 3.7003, 0.0001);
+        Assert.assertEquals(vol.get("B"), 0.9014, 0.0001);
+    }
+    
+    @Test
+    public void seriesAverage() {
+        
+        var avgVal = Metrics.average(quoteSeries);
+        
+        final Quote exp = new Quote(Map.of("A", 24.65, "B", 10.75));
+        Assert.assertEquals(avgVal, exp);
+    }
+}
