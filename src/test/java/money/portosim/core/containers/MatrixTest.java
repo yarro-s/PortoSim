ce255eebbc5c39bc493ce85493aef6a64f824cfe
yarro <yarro@localhost>
2022-06-27 20:07:53 +0300

    add Matrix tests

--- src/test/java/money/portosim/core/containers/MatrixTest.java
+++ src/test/java/money/portosim/core/containers/MatrixTest.java
@@ -8,25 +8,22 @@ import java.util.Map;
 import money.portosim.containers.core.DataMatrix;
 import money.portosim.containers.numeric.NumDataMatrix;
 import money.portosim.containers.core.Pair;
-import money.portosim.containers.core.DataRecord;
 import money.portosim.containers.core.Frame;
 import money.portosim.containers.core.Matrix;
-import money.portosim.containers.core.OrderedFrame;
 import org.testng.Assert;
-import static org.testng.Assert.*;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
  *
  * @author yarro
  */
-public class DataFrameTest {
+public class MatrixTest {
     
-    final Frame<Pair<Integer, String>, Double> tf;
+    final Matrix<Integer, String, Double> tf = new DataMatrix<>();
 
-    public DataFrameTest() {
-        this.tf = new DataMatrix<>();
-        
+    @BeforeClass
+    public void setUp() {
         tf.put(Pair.of(1, "A"), 100.5); tf.put(Pair.of(1, "B"), 6.2);
         tf.put(Pair.of(2, "A"), 245.0); tf.put(Pair.of(2, "B"), 8.1);
         tf.put(Pair.of(3, "A"), 307.4); tf.put(Pair.of(3, "B"), 2.8);
@@ -37,8 +34,10 @@ public class DataFrameTest {
     @Test
     public void sumAllFrame() {
         var nf = new NumDataMatrix<>(tf);
+        double frameSum = nf.sum();
         
-        System.out.println("\n >>>>" + nf.sum());
+        Assert.assertEquals(frameSum, 100.5 + 245.0 + 307.4 + 843.2 + 367.4 + 
+                6.2 + 8.1 + 2.8 + 5.0 + 1.5, 10e-6);
     }
     
     @Test
@@ -52,36 +51,45 @@ public class DataFrameTest {
         otherNM.put(Pair.of(4, "A"), 43.0); otherNM.put(Pair.of(4, "B"), 15.0);
         otherNM.put(Pair.of(5, "A"), 32.4); otherNM.put(Pair.of(5, "B"), 125.5);
         
-        System.out.println("\n >>>>" + nf.add(otherNM));
+        var addedFrame = new NumDataMatrix<>(nf.add(otherNM));
+        
+        Assert.assertEquals(addedFrame.columns().get("A").get(1), 100.5 + 64.1);
+        Assert.assertEquals(addedFrame.rows().get(5).get("B"), 1.5 + 125.5);
     }
        
     @Test
     public void addVal() {
         var nm = new NumDataMatrix<>(tf);
-        System.out.println("\n >>>>" + nm.add(50000.6));
+        var addedVal = new NumDataMatrix<>(nm.add(50000.6));
+        
+        Assert.assertEquals(addedVal.columns().get("A").get(1), 100.5 + 50000.6);
+        Assert.assertEquals(addedVal.rows().get(5).get("B"), 1.5 + 50000.6);
     }
     
     @Test
-    public void matrixInit() {
+    public void matrixDataAccess() {
         final Matrix<Integer, String, Double> mx = new DataMatrix<>(Map.of(
                 Pair.of(1, "A"), 100.5, Pair.of(1, "B"), 6.2,
                 Pair.of(2, "A"), 245.0, Pair.of(2, "B"), 8.1,
                 Pair.of(3, "A"), 307.4, Pair.of(3, "B"), 2.8,
                 Pair.of(4, "A"), 843.2, Pair.of(4, "B"), 5.0,
-                Pair.of(5, "A"), 367.4, Pair.of(5, "B"), 1.5));     
-        
-//        mx.put(Pair.of(1000, "APMP"), 9087435.2);
-//        mx.columns().get("A").put(50424, 12368534.64);
-        
+                Pair.of(5, "A"), 367.4, Pair.of(5, "B"), 1.5));            
         
         mx.rows().get(4).put("A", -4243.25);
         
-//        System.out.println("\n>>>>>> " + mx);
+        Assert.assertEquals(mx.columns().get("A").get(4), mx.rows().get(4).get("A"));
+        Assert.assertEquals(mx.rows().get(4).get("A"), -4243.25);
+        
+        mx.columns().get("B").put(3, 35.56);
+        
+        Assert.assertEquals(mx.columns().get("B").get(3), mx.rows().get(3).get("B"));
+        Assert.assertEquals(mx.columns().get("B").get(3), 3, 35.56);
     }
     
     @Test
-    public void dataFrameInit() {
-
-//        System.out.println("\n>>>>> " + tf.get(Pair.of(3, "A")));
+    public void matrixInit() {     
+        Assert.assertEquals(tf.columns().size(), 2);
+        Assert.assertEquals(tf.rows().size(), 5);
+        Assert.assertEquals(tf.size(), 10);
     }
 }
